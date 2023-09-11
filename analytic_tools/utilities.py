2fbe89a54c221eb478b7cffb7baef755a8507e3a
Tonje Viddal Sandanger <tonjevs@ulrik.uio.no>
2023-09-11 12:20:51 +0200

    Updated files with doc_strings

--- analytic_tools/utilities.py
+++ analytic_tools/utilities.py
@@ -4,6 +4,17 @@ from typing import Dict, List
 
 
 def get_diagnostics(dir: str or Path) -> Dict[str, int]:
+    """Get diagnostics for the directory tree, with root directory pointed to by dir.
+       Counts up all the files, subdirectories, and specifically .csv, .txt, .npy, .md and other files in the whole directory tree.
+
+    Parameters:
+        dir (str or pathlib.Path) : Absolute path to the directory of interest
+
+    Returns:
+        res (Dict[str, int]) : a dictionary of the findings with following keys: files, subdirectories, .csv files, .txt files, .npy files, .md files, other files.
+
+    """
+
     res = {
         "files": 0,
         "subdirectories": 0,
@@ -46,6 +57,30 @@ def get_diagnostics(dir: str or Path) -> Dict[str, int]:
 
 
 def display_diagnostics(dir: str or Path, contents: Dict[str, int]) -> None:
+    """Display diagnostics for the directory tree, with root directory pointed to by dir.
+        Objects to display: files, subdirectories, .csv files, .txt files, .npy files, .md files, other files.
+
+    Parameters:
+        dir (str or pathlib.Path) : Absolute path the directory of interest
+        contents (Dict[str, int]) : a dictionary of the same type as return type of get_diagnostics, has the form:
+
+            .. highlight:: python
+            .. code-block:: python
+
+                {
+                    "files": 0,
+                    "subdirectories": 0,
+                    ".csv files": 0,
+                    ".txt files": 0,
+                    ".npy files": 0,
+                    ".md files": 0,
+                    "other files": 0,
+                }
+
+    Returns:
+        None
+    """
+
     if not isinstance(dir, (str,Path)):
         raise TypeError("Invalid type for directiory. Expected a path...")
     
@@ -66,6 +101,18 @@ def display_diagnostics(dir: str or Path, contents: Dict[str, int]) -> None:
         print(f"{fileType}: {number}")
 
 def display_directory_tree(dir: str or Path, maxfiles: int = 3) -> None:
+    """Display a directory tree, with root directory pointed to by dir.
+       Limit the number of files to be displayed for convenience to maxfiles.
+       This tree is built with inspiration from the code written by "Flimm" at https://stackoverflow.com/questions/6639394/what-is-the-python-way-to-walk-a-directory-tree
+
+    Parameters:
+        dir (str or pathlib.Path) : Absolute path to the directory of interest
+        maxfiles (int) : Maximum number of files to be displayed at each level in the tree, default to three.
+
+    Returns:
+        None
+
+    """
     if not isinstance(dir, (str,Path)):
         raise TypeError("Invalid type for directiory. Expected a path...")
     
@@ -99,6 +146,16 @@ def display_directory_tree(dir: str or Path, maxfiles: int = 3) -> None:
                 print(f'   - ({len(subdir) - maxfiles} more)')
                
 def is_gas_csv(path: str or Path) -> bool:
+    """Checks if a csv file pointed to by path is an original gas statistics file.
+        An original file must be called '[gas_formula].csv' where [gas_formula] is
+        in ['CO2', 'CH4', 'N2O', 'SF6', 'H2'].
+
+    Parameters:
+         - path (str of pathlib.Path) : Absolute path to .csv file that will be checked
+
+    Returns
+         - (bool) : Truth value of whether the file is an original gas file
+    """
 
     if not isinstance(path, (str,Path)):
         raise TypeError("Invalid type for directiory. Expected a path...")
@@ -119,7 +176,20 @@ def is_gas_csv(path: str or Path) -> bool:
 
 
 def get_dest_dir_from_csv_file(dest_parent: str or Path, file_path: str or Path) -> Path:
-   
+    """Given a file pointed to by file_path, derive the correct gas_[gas_formula] directory name.
+        Checks if a directory "gas_[gas_formula]", exists and if not, it creates one as a subdirectory under dest_parent.
+
+        The file pointed to by file_path must be a valid file. A valid file must be called '[gas_formula].csv' where [gas_formula]
+        is in ['CO2', 'CH4', 'N2O', 'SF6', 'H2'].
+
+    Parameters:
+        - dest_parent (str or pathlib.Path) : Absolute path to parent directory where gas_[gas_formula] should/will exist
+        - file_path (str or pathlib.Path) : Absolute path to file that gas_[gas_formula] directory will be derived from
+
+    Returns:
+        - (pathlib.Path) : Absolute path to the derived directory
+
+    """
     dest_parent = Path(dest_parent)
     dest_path = Path(file_path)
 
@@ -150,6 +220,15 @@ def get_dest_dir_from_csv_file(dest_parent: str or Path, file_path: str or Path)
 
 
 def merge_parent_and_basename(path: str or Path) -> str:
+    """This function merges the basename and the parent-name of a path into one, uniting them with "_" character.
+       It then returns the basename of the resulting path.
+
+    Parameters:
+        - path (str or pathlib.Path) : Absolute path to modify
+
+    Returns:
+        - new_base (str) : New basename of the path
+    """
     path = Path(path)
 
     if not isinstance(path,Path): 
@@ -167,6 +246,16 @@ def merge_parent_and_basename(path: str or Path) -> str:
 
 
 def delete_directories(path_list: List[str or Path]) -> None:
+    """Prompt the user for permission and delete the objects pointed to by the paths in path_list if
+       permission is given. If the object is a directory, its whole directory tree is removed.
+
+    Parameters:
+        - path_list (List[str | Path]) : a list of absolute paths to all the objects to be removed.
+
+
+    Returns:
+    None
+    """
     #Tror ikke denne fungerer helt riktig men det må jo være noe sånt
     for path in path_list: 
         path = Path(path)
--- analyze_pollution_data.py
+++ analyze_pollution_data.py
@@ -19,7 +19,26 @@ from analytic_tools.plotting import(
 )
 
 def restructure_pollution_data(pollution_dir: str or Path, dest_dir: str or Path) -> None:
-    
+    """This function searches the tree of pollution_data directory pointed to by pollution_dir for .csv files
+        that satisfy the criteria described in the assignment. It then moves a renamed copy of these files to gas-specific
+        sub-directories in dest_dir, which will be created based on the gasses present in pollution_data directory.
+
+    Parameters:
+        - pollution_dir (str or pathlib.Path) : The absolute path to pollution_data directory
+        - dest_dir (str or pathlib.Path) : The absolute path to new directory where gas-specific subdirectories will
+                                     be created, which must be pollution_data_restructured/by_gas
+
+    Returns:
+    None
+
+    Pseudocode:
+    1. Iterate through the contents of `pollution_dir`
+    2. Find valid .csv files for gasses ([`[gas_formula].csv` files of correct gas types).
+    3. Create/assign new directory to store them under `dest_dir` using `get_dest_dir_from_csv_file`
+    4. Assign a new name using `merge_parent_and_basename` and copy the file to the new destination.
+       If the file happens already to exist there, it should be overwritten.
+    """
+
     if not isinstance(pollution_dir,(str,Path)) or not isinstance(dest_dir,(str,Path)): 
         raise TypeError("Object is not path-like")
 
@@ -42,7 +61,25 @@ def restructure_pollution_data(pollution_dir: str or Path, dest_dir: str or Path
             print(e)
 
 def analyze_pollution_data(work_dir: str or Path) -> None:
-    
+    """Do the restructuring of the pollution_data and plot
+       the statistics showing emissions of each gas as function of all the corresponding
+       sources. The new structure and the plots are saved in a separate directory under work_dir
+
+    Parameters:
+        - work_dir (str or pathlib.Path) : Absolute path to the working directory that
+                                    contains the pollution_data directory and where the new directories will be created
+
+    Returns:
+    None
+
+    Pseudocode:
+    - Create pollution_data_restructured in work_dir
+    - Populate it with a by_gas subdirectory
+    - Make a call to restructure_pollution_data
+    - Populate pollution_data_restructured with a subdirectory named figures
+    - Make a call to plot_pollution_data
+    """
+
     if not isinstance(work_dir, (str,Path)): 
         raise TypeError(f'{work_dir} is not a Path-like object')
     
@@ -70,9 +107,24 @@ def analyze_pollution_data(work_dir: str or Path) -> None:
 
     plot_pollution_data(by_gas_dir, figures_dir)
 
-
-
 def analyze_pollution_data_tmp(work_dir: str or Path) -> None:
+    """Do the restructuring of the pollution_data in a temporary directory and create the figures
+       showing emissions of each gas as function of all the corresponding
+       sources. The new figures are saved in a real directory under work_dir.
+
+    Parameters:
+        - work_dir (str or pathlib.Path) : Absolute path to the working directory that
+                                    contains the pollution_data directory and where the figures will be saved
+
+    Returns:
+    None
+
+    Pseudocode:
+    - Create a temporary directory and copy pollution_data directory to it
+    - Perform the same operations as in analyze_pollution_data
+    - Copy (or directly save) the figures to a directory named `figures` under the original working directory pointed to by `work_dir`
+    """
+     
     if not isinstance(work_dir, (str,Path)): 
         raise TypeError(f'{work_dir} is not a Path-like object')
     
